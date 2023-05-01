def04c00016b6a9d1bead7f2063cf988a182c185
Sumit Kumar Dey <sumit.dey@publicissapient.com>
2023-05-01 21:10:03 +0100

    Springboot project created

---
+++ SpringBoot_CountriesFromJSON/src/main/java/SpringBoot_CountriesFromJSON/SpringBootCountriesFromJsonApplication.java
@@ -0,0 +1,14 @@
+package SpringBoot_CountriesFromJSON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCountriesFromJsonApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCountriesFromJsonApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_CountriesFromJSON/src/test/java/SpringBoot_CountriesFromJSON/SpringBootCountriesFromJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot_CountriesFromJSON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCountriesFromJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
